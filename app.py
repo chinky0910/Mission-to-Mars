f076ce0755989a09d7eece37f84457e3241c6f4f
Chinky Gupta <95595378+chinky0910@users.noreply.github.com>
2022-03-13 23:44:08 -0500

    Updating app.py

--- app.py
+++ app.py
@@ -1,77 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Mission to Mars</title>
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    />
-  </head>
-  <body>
-    <div class="container">
-      <!-- Add Jumbotron to Header -->
-      <div class="jumbotron text-center" style="background-color: #f3d6ca">
-        <h1 style="color: #d16b3b">Mission to Mars</h1>
-        <!-- Add a button to activate scraping script -->
-        <p><a class="btn btn-danger btn-lg" href="/scrape" role="button">Scrape New Data</a></p>
-      </div>
+#Import Dependencies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
 
-      <!-- Add section for Mars News -->
-      <div class="row" id="mars-news">
-        <div class="col-sm-12">
-          <div class="media">
-            <div class="media-body">
-              <h2>Latest Mars News</h2>
-              <h4 class="media-heading">{{ mars.news_title }}</h4>
-              <p>{{ mars.news_paragraph }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
+#Set up Flask
+app = Flask(__name__)
 
-      <!-- Section for Featured Image and Facts table -->
-      <div class="row" id="mars-featured-image">
-        <div class="col-md-8">
-          <h2>Featured Mars Image</h2>
+#Connect to Mongo using PyMongo
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
-          <!-- if images is False/None/non-existent, then default to error message -->
-          <img
-            src="{{mars.featured_image | default('static/images/error.png', true) }}"
-            class="img-responsive"
-            alt="Responsive image"
-          />
-        </div>
+#Define the route for the HTML page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
-        <div class="col-md-4">
-          <!-- Mars Facts -->
-          <div class="row" id="mars-facts" >
-            <h2>Mars Facts</h2>
-             <table border="5"  style="background-color: #f3d6ca" class="dataframe table table-striped" 
-             src="{{ mars.facts | safe }}"></table>
-          </div>
-        </div>
-      </div>
+#Add next route and function to our code
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
 
-      <!-- Section for Mars Hemispheres -->
-      <div class="row" id="mars-hemispheres">
-        <div class="page-header">
-          <h2 class="text-center">Mars Hemispheres</h2>
-        </div>
-
-        {% for hemisphere in mars.hemispheres %}
-        <div class="col-md-3">
-          <div class="img-thumbnail">
-            <img src="{{hemisphere.img_url | default('static/images/error.png', true)}}" alt="..." class="img-thumbnail">
-            <div class="caption">
-              <h3>{{hemisphere.title}}</h3>
-            </div>
-          </div>
-        </div>
-        {% endfor %}
-      </div>
-    </div>
-  </body>
-</html>
+#Run the code
+if __name__ == "__main__":
+   app.run()
